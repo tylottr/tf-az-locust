0c12dd8a4755e07b22fc295661f667304e7c3de9
Ty <tyla.boff@outlook.com>
2020-02-07 20:26:21 +0000

    updated access output

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,10 @@ output "server_vm_info" {
 
 output "server_vm_web_access" {
   description = "Information to log into the Locust Server"
-  value = "To access the server run `ssh ${var.vm_username}@${azurerm_public_ip.main_server.fqdn} -L 8080:localhost:8089` to and open your browser to http://localhost:8080"
+  value = <<EOF
+To access the server follow the below steps:
+1. Run the below command
+  ssh ${var.vm_username}@${azurerm_public_ip.main_server.fqdn} -i ${local_file.main_ssh_private.filename} -L 8080:localhost:8089
+2. Open your browser to http://localhost:8080
+EOF
 }
